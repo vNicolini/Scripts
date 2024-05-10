29c4f43b485e668c5b4ab72d627f522a0e24b680
vNicolini <57097563+vNicolini@users.noreply.github.com>
2024-05-10 14:59:00 +0200

    Update ACES_to_TEX.bat
    added missing mode flag to periodic

--- txmake/ACES_to_TEX.bat
+++ txmake/ACES_to_TEX.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-@for %%i in (%*) do ("G:\pipeline\renderers\Prman\RenderManProServer-26.0\bin\txmake.exe" -verbose -t:4 -ocioconfig %OCIO% -format pixar %%i %%i.tex)
+@for %%i in (%*) do ("G:\pipeline\renderers\Prman\RenderManProServer-26.0\bin\txmake.exe" -verbose -t:4 -mode periodic -ocioconfig %OCIO% -format pixar %%i %%i.tex)
 pause
